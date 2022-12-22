0e0a38bba7c1d04c52af50d6c1d9f0b5a1585e88
OscarwOw <OscarwOw210@gmail.com>
2022-12-22 01:09:10 +0100

    Add form to registration and login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,1 +1,38 @@
-console.log("Hello World!")
+import React, { useState } from 'react';
+
+function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        // You can now use the `username` and `password` variables
+        // to send a request to your server to log in the user
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={handleUsernameChange} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" value={password} onChange={handlePasswordChange} />
+            </label>
+            <br />
+            <button type="submit">Log In</button>
+        </form>
+    );
+}
+
+export default LoginPage;
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,1 +1,48 @@
-console.log("Hello World!")
+import React, { useState } from 'react';
+
+function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value);
+    }
+
+    function handleEmailChange(event) {
+        setEmail(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        // You can now use the `username`, `email`, and `password` variables
+        // to send a request to your server to register the user
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={handleUsernameChange} />
+            </label>
+            <br />
+            <label>
+                Email:
+                <input type="email" value={email} onChange={handleEmailChange} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" value={password} onChange={handlePasswordChange} />
+            </label>
+            <br />
+            <button type="submit">Register</button>
+        </form>
+    );
+}
+
+export default RegisterPage;
