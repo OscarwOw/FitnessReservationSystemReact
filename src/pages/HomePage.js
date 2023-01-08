c804286af01dd783ca93d0303be08d8953b540da
OscarwOw <OscarwOw210@gmail.com>
2023-01-08 14:41:25 +0100

    nabigation bar color and always top

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,6 +7,40 @@ function HomePage() {
             <p>We are dedicated to providing a comfortable and supportive environment for individuals of all fitness levels to achieve their health and wellness goals. Our state-of-the-art facility offers a wide range of equipment and classes, including cardio machines, weight training equipment, group fitness classes, and personal training services.</p>
             <p>We also have a sauna, steam room, and hot tub for members to relax and unwind after a workout. Our team of professional and friendly staff are always available to assist you with any questions or concerns.</p>
             <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
+            <p>We look forward to seeing you at the gym!</p>
         </div>
     );
 }
