07165860d91eb7495fec0351011b43e83f029e73
OscarwOw <OscarwOw210@gmail.com>
2023-02-01 22:53:09 +0100

    adding fa to footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,21 +1,25 @@
 import styles from './Footer.module.css';
+import {FaYoutube, FaTwitter, FaFacebook, } from 'react-icons/fa'
 
 function Footer(){
 
     return(
         <div className={styles.container}>
             <div className={styles.column}>
-                <h1>Section one</h1>
-                <ul>
-                    <li></li>
+                <h2>Contact</h2>
+                <p>17. listopadu 1790/5</p>
+                <p>oscarwOw210@gmail.com</p>
+                
+                
+            </div>
+            <div className={styles.column}>
+            <ul className={styles.social}>
+                    <li><FaYoutube size={'3em'}/></li>
+                    <li><FaTwitter size={'3em'}/></li>
+                    <li><FaFacebook size={'3em'}/></li>
+                    
                     
                 </ul>
-            </div>
-            <div className={styles.column}>
-                <h1>section two</h1>
-            </div>
-            <div className={styles.column}>
-            <h1>section three</h1>
             </div>
         </div>
         )
